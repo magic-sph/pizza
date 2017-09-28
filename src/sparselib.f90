7a0ab77fb883dd4b105a8f894c1a72fbae20a88c
Thomas Gastine <gastine@mps.mpg.de>
2017-09-28 14:18:03 +0200

    biharmonic sparse operator OK!

--- src/sparselib.f90
+++ src/sparselib.f90
@@ -1,13 +1,13 @@
 module sparselib
 
    use precision_mod
-   use constants, only: half
+   use constants, only: half, one
 
    implicit none
 
    private
 
-   public :: r1, i1, i2r1
+   public :: r1, i1, i2, i4, i2r1, eye
 
 contains
 
@@ -56,6 +56,84 @@ contains
       stencil(ku+3:len_stencil)= 0.0_cp
 
    end function i1
+!------------------------------------------------------------------------------
+   function i2(a, idx, len_stencil) result(stencil)
+
+      !-- Inputvariable
+      real(cp), intent(in) :: a
+      integer,  intent(in) :: idx
+      integer,  intent(in) :: len_stencil
+
+      !-- Output variable
+      real(cp) :: stencil(len_stencil)
+
+      !-- Local variables
+      integer :: ku
+
+      ku = (len_stencil-1)/2
+
+      stencil(1:ku-1)          = 0.0_cp
+      stencil(ku-1)            = a*a/4.0_cp/real(idx,cp)/real(idx+1,cp)
+      stencil(ku)              = 0.0_cp
+      stencil(ku+1)            =-a*a/2.0_cp/real(idx-1,cp)/real(idx+1,cp)
+      stencil(ku+2)            = 0.0_cp
+      stencil(ku+3)            = a*a/4.0_cp/real(idx,cp)/real(idx-1,cp)
+      stencil(ku+4:len_stencil)= 0.0_cp
+
+   end function i2
+!------------------------------------------------------------------------------
+   function eye(len_stencil) result(stencil)
+
+      !-- Inputvariable
+      integer,  intent(in) :: len_stencil
+
+      !-- Output variable
+      real(cp) :: stencil(len_stencil)
+
+      !-- Local variables
+      integer :: ku
+
+      ku = (len_stencil-1)/2
+
+      stencil(1:ku)            =0.0_cp
+      stencil(ku+1)            =one      ! Diagonal
+      stencil(ku+2:len_stencil)=0.0_cp
+
+   end function eye
+!------------------------------------------------------------------------------
+   function i4(a, idx, len_stencil) result(stencil)
+
+      !-- Inputvariable
+      real(cp), intent(in) :: a
+      integer,  intent(in) :: idx
+      integer,  intent(in) :: len_stencil
+
+      !-- Output variable
+      real(cp) :: stencil(len_stencil)
+
+      !-- Local variables
+      integer :: ku
+
+      ku = (len_stencil-1)/2
+
+      stencil(1:ku-1)          = 0.0_cp
+      stencil(ku-3)            = a*a*a*a/16.0_cp/real(idx,cp)/real(idx+1,cp)/&
+      &                          real(idx+2,cp)/real(idx+3,cp)
+      stencil(ku-2)            = 0.0_cp
+      stencil(ku-1)            =-a*a*a*a/4.0_cp/real(idx,cp)/real(idx-1,cp)/ &
+      &                          real(idx+1,cp)/real(idx+3,cp)
+      stencil(ku)              = 0.0_cp
+      stencil(ku+1)            = 3.0_cp*a*a*a*a/8.0_cp/real(idx-2,cp)/     &
+      &                          real(idx-1,cp)/real(idx+1,cp)/real(idx+2,cp)
+      stencil(ku+2)            = 0.0_cp
+      stencil(ku+3)            =-a*a*a*a/4.0_cp/real(idx,cp)/real(idx-3,cp)/ &
+      &                          real(idx-1,cp)/real(idx+1,cp)
+      stencil(ku+4)            = 0.0_cp
+      stencil(ku+5)            = a*a*a*a/16.0_cp/real(idx,cp)/real(idx-3,cp)/ &
+      &                          real(idx-2,cp)/real(idx-1,cp)
+      stencil(ku+6:len_stencil)= 0.0_cp
+
+   end function i4
 !------------------------------------------------------------------------------
    function i2r1(a, b, idx, len_stencil) result(stencil)
 
