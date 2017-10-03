648c063658c45e498e87854ee953fb1e3242309f
Thomas Gastine <gastine@mps.mpg.de>
2017-10-03 18:14:05 +0200

    add a preliminary type for bordered matrix

--- src/matrix_types.f90
+++ src/matrix_types.f90
@@ -18,15 +18,34 @@ module matrix_types
 
    contains
 
-      procedure :: initialize
-      procedure :: finalize
+      procedure :: initialize => initialize_band
+      procedure :: finalize => finalize_band
       procedure :: mat_vec_mul
 
    end type type_bandmat_real
 
+   type, public :: type_bordmat_real
+
+      integer :: kl ! Number of lower diagonals
+      integer :: ku ! Number of upper diagonals
+      integer :: nbands ! Number of bands
+      integer :: nlines_band ! Number of lines
+      integer :: ntau        ! Number of tau lines
+      real(cp), allocatable :: A1(:,:) ! Upper left block
+      real(cp), allocatable :: A2(:,:) ! Upper right block
+      real(cp), allocatable :: A3(:,:) ! Lower left block
+      real(cp), allocatable :: A4(:,:) ! Lower right block
+
+   contains
+
+      procedure :: initialize => initialize_bord
+      procedure :: finalize => finalize_bord
+
+   end type type_bordmat_real
+
 contains
 
-   subroutine initialize(this, kl, ku, len)
+   subroutine initialize_band(this, kl, ku, len)
 
       class(type_bandmat_real) :: this
 
@@ -54,15 +73,15 @@ contains
 
       bytes_allocated = bytes_allocated+this%nbands*this%nlines*SIZEOF_DEF_REAL
 
-   end subroutine initialize
+   end subroutine initialize_band
 !------------------------------------------------------------------------------
-   subroutine finalize(this)
+   subroutine finalize_band(this)
 
       class(type_bandmat_real) :: this
 
       deallocate( this%dat )
 
-   end subroutine finalize
+   end subroutine finalize_band
 !------------------------------------------------------------------------------
    subroutine mat_vec_mul(this, vec)
 
@@ -93,5 +112,63 @@ contains
       end do
 
    end subroutine mat_vec_mul
+!------------------------------------------------------------------------------
+   subroutine initialize_bord(this, kl, ku, nbounds, nlines)
+
+      class(type_bordmat_real) :: this
+
+      !-- Input variables
+      integer, intent(in) :: kl
+      integer, intent(in) :: ku
+      integer, intent(in) :: nbounds
+      integer, intent(in) :: nlines
+
+      !-- Local variables
+      integer :: n_r, n_b
+
+      this%kl = kl
+      this%ku = ku
+      this%ntau = nbounds
+      this%nlines_band = nlines-nbounds
+      this%nbands = this%kl+this%ku+1
+
+      allocate( this%A1(this%ntau, this%ntau) )
+      allocate( this%A2(this%ntau, this%nlines_band) )
+      allocate( this%A3(this%nlines_band, this%ntau) )
+      allocate( this%A4(this%nbands+this%kl, this%nlines_band) )
+
+      do n_r=1,nlines
+         do n_b=1,this%ntau
+            if ( n_r <= this%ntau ) then
+               this%A1(n_b,n_r)=0.0_cp
+            else
+               this%A2(n_b,n_r-this%ntau)=0.0_cp
+            end if
+         end do
+      end do
+
+      do n_r=1,this%nlines_band
+         do n_b=1,this%ntau
+            this%A3(n_b,n_r)=0.0_cp
+         end do
+         do n_b=1,this%nbands+this%kl
+            this%A4(n_b,n_r)=0.0_cp
+         end do
+      end do
+
+      bytes_allocated=bytes_allocated+this%ntau*nlines*SIZEOF_DEF_REAL+&
+      &               this%ntau*this%nlines_band*SIZEOF_DEF_REAL+      &
+      &               (this%nbands+this%kl)*this%nlines_band*          &
+      &               SIZEOF_DEF_REAL
+
+   end subroutine initialize_bord
+!------------------------------------------------------------------------------
+   subroutine finalize_bord(this)
+
+      class(type_bordmat_real) :: this
+
+      deallocate( this%A1, this%A2, this%A3, this%A4 )
+
+   end subroutine finalize_bord
 !------------------------------------------------------------------------------
 end module matrix_types
