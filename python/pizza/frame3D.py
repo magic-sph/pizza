aa7be6f6ae7cd6f7954ca5dcfb02200d6316528d
Thomas Gastine <gastine@ipgp.fr>
2019-04-12 13:36:33 +0200

    python support for plotting 3-D fields

--- python/pizza/frame3D.py
+++ python/pizza/frame3D.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-from .libpizza import scanDir
+from .libpizza import scanDir, symmetrize
+from .frame import my_interp2d
+from .plotlib import equatContour, merContour, radialContour
 from .log import PizzaSetup
 import os
 import re
@@ -32,6 +34,8 @@ class Frame3D:
                                      count=1)[0]
         self.tcond_3D = np.fromfile(file, dtype='%iFloat64' % self.n_r_max_3D,
                                     count=1)[0]
+        self.theta = np.fromfile(file, dtype='%iFloat64' % self.n_theta_max,
+                                 count=1)[0]
 
         dt = np.dtype("(%i,%i,%i)Float32" % (self.n_r_max_3D,
                                              self.n_theta_max,
@@ -77,6 +81,7 @@ class Pizza3DFields(PizzaSetup):
         self.n_theta_max = f.n_theta_max
         self.n_phi_max_3D = f.n_phi_max_3D
         self.radius_3D = f.radius_3D
+        self.theta = f.theta
         self.tcond_3D = f.tcond_3D
         self.time = f.time
 
@@ -155,3 +160,207 @@ class Pizza3DFields(PizzaSetup):
         if verbose:
             print('read %s' % filename)
         return filename
+
+    def avg(self, field='up', cm='seismic', levels=65, normed=True, vmax=None,
+            vmin=None, cbar=True, label=None):
+        """
+        Display an azimuthal average of a scalar quantity
+
+        :param field: the name of the input physical quantity you want to
+                      display
+        :type field: str
+        :param levels: the number of levels in the contour
+        :type levels: int
+        :param cm: name of the colormap ('jet', 'seismic', 'RdYlBu_r', etc.)
+        :type cm: str
+        :param cbar: display the colorbar when set to True
+        :type cbar: bool
+        :param vmax: maximum value of the contour levels
+        :type vmax: float
+        :param vmin: minimum value of the contour levels
+        :type vmin: float
+        :param normed: when set to True, the colormap is centered around zero.
+                       Default is True, except for entropy/temperature plots.
+        :type normed: bool
+        """
+
+        if field in ('temperature', 'Temperature', 'temp', 'Temp', 't', 'T'):
+            data = self.temp.mean(axis=0)
+        elif field in ('ur', 'Ur', 'Vr', 'vr'):
+            data = self.ur.mean(axis=0)
+        elif field in ('ut', 'Ut', 'utheta', 'Utheta', 'vt', 'Vt', 'Vtheta',
+                       'vtheta'):
+            data = self.utheta.mean(axis=0)
+        elif field in ('up', 'Up', 'uphi', 'Uphi', 'vp', 'Vp', 'Vphi', 'vphi'):
+            data = self.uphi.mean(axis=0)
+
+        self.fig, xx, yy = merContour(data, self.radius_3D, colat=self.theta,
+                                      label=label,
+                                      levels=levels, cm=cm, normed=normed,
+                                      vmax=vmax, vmin=vmin, cbar=cbar,
+                                      tit=False)
+
+    def equat(self, field='ur', cm='seismic', levels=65, deminc=True,
+              normed=True, vmax=None, vmin=None, normRad=False, stream=False,
+              streamNorm='vel', streamDensity=1.5, cbar=True, label=None,
+              streamColor='k'):
+        """
+        Display an equatorial cut of a scalar quantity
+
+        :param field: the name of the input physical quantity you want to
+                      display
+        :type field: str
+        :param normRad: when set to True, the contour levels are normalised
+                        radius by radius (default is False)
+        :type normRad: bool
+        :param levels: the number of levels in the contour
+        :type levels: int
+        :param cm: name of the colormap ('jet', 'seismic', 'RdYlBu_r', etc.)
+        :type cm: str
+        :param tit: display the title of the figure when set to True
+        :type tit: bool
+        :param cbar: display the colorbar when set to True
+        :type cbar: bool
+        :param vmax: maximum value of the contour levels
+        :type vmax: float
+        :param vmin: minimum value of the contour levels
+        :type vmin: float
+        :param normed: when set to True, the colormap is centered around zero.
+                       Default is True, except for entropy/temperature plots.
+        :type normed: bool
+        :param stream: a boolean to control the plotting of streamlines (False
+                       by default)
+        :type stream: bool
+        :param streamNorm: scalar that is used to normalise the thickness of
+                           the streamlines (by default 'vel')
+        :type streamNorm: str
+        :param streamDensity: control the number of streamlines
+        :type streamDensity: float
+        :param streamColor: color of the streamlines
+        :type streamColor: str
+        """
+
+        if field in ('temperature', 'Temperature', 'temp', 'Temp', 't', 'T'):
+            data = self.temp[:, self.n_theta_max/2, :]
+        elif field in ('tfluct', 'tempfluct'):
+            data = self.temp-self.temp.mean(axis=0)
+            data = data[:, self.n_theta_max/2, :]
+        elif field in ('ur', 'Ur', 'Vr', 'vr'):
+            data = self.ur[:, self.n_theta_max/2, :]
+        elif field in ('ut', 'Ut', 'utheta', 'Utheta', 'vt', 'Vt', 'Vtheta',
+                       'vtheta'):
+            data = self.utheta[:, self.n_theta_max/2, :]
+        elif field in ('up', 'Up', 'uphi', 'Uphi', 'vp', 'Vp', 'Vphi', 'vphi'):
+            data = self.uphi[:, self.n_theta_max/2, :]
+
+        if deminc:
+            data = symmetrize(data, ms=self.minc_3D)
+
+        self.fig, xx, yy = equatContour(data, self.radius_3D,
+                                        minc=self.minc_3D, levels=levels,
+                                        cm=cm, deminc=deminc, normed=normed,
+                                        vmax=vmax, vmin=vmin, normRad=normRad,
+                                        cbar=cbar, label=label)
+
+        if stream:
+            ax = self.fig.get_axes()[0]
+            bbox = ax.get_position()
+            # For now matplotlib polar plots do not handle theta_min
+            # theta_max (set_xlim does not work)
+            # As a trick I simply shift the polar Axes outside the domain
+            # when minc /= 1
+            if not deminc:
+                a = bbox.bounds[0]
+                b = bbox.bounds[1]
+                c = bbox.bounds[2]
+                d = bbox.bounds[3]
+                if self.minc == 2:
+                    bbox.bounds = (a, b-d, c, 2*d)
+                elif self.minc == 4:
+                    bbox.bounds = (a-c, b-d, 2*c, 2*d)
+            ax1 = self.fig.add_axes(bbox, polar=True)
+            ax1.axis('off')
+
+            if deminc:
+                # theta = np.linspace(-np.pi, np.pi, data.shape[0])
+                phi = np.linspace(0., 2.*np.pi, data.shape[0])
+            else:
+                phi = np.linspace(0., 2*np.pi/self.minc_3D, data.shape[0])
+
+            rad = np.linspace(self.radius_3D[0], self.radius_3D[-1],
+                              data.shape[-1])
+            rr, pphi = np.meshgrid(rad, phi)
+            if deminc:
+                u = symmetrize(self.ur[:, self.n_theta_max/2, :], self.minc_3D)
+                v = symmetrize(self.uphi[:, self.n_theta_max/2, :],
+                               self.minc_3D)
+            else:
+                u = self.ur[:, self.n_theta_max/2, :]
+                v = self.uphi[:, self.n_theta_max/2, :]
+            v /= self.radius_3D
+
+            u = my_interp2d(u, self.radius_3D, rad)
+            v = my_interp2d(v, self.radius_3D, rad)
+            speed = np.sqrt(u**2+v**2)
+
+            if streamNorm == 'vel':
+                lw = 3.*speed.T/speed.max()
+            ax1.streamplot(pphi.T, rr.T, v.T, u.T, density=streamDensity,
+                           linewidth=lw, color=streamColor)
+            ax1.set_ylim(0., rad.max())
+
+    def surf(self, r=0.5, field='ur', cm='seismic', levels=65, deminc=True,
+             normed=True, vmax=None, vmin=None, cbar=True, label=None,
+             streamColor='k', lon_0=0, lines=False):
+        """
+        Display an radial cut of a scalar quantity
+
+        :param field: the name of the input physical quantity you want to
+                      display
+        :type field: str
+        :param r: radius at which the cut is done
+        :type r: float
+        :param lon_0: the central longitude
+        :type lon_0: float
+        :param levels: the number of levels in the contour
+        :type levels: int
+        :param cm: name of the colormap ('jet', 'seismic', 'RdYlBu_r', etc.)
+        :type cm: str
+        :param tit: display the title of the figure when set to True
+        :type tit: bool
+        :param cbar: display the colorbar when set to True
+        :type cbar: bool
+        :param vmax: maximum value of the contour levels
+        :type vmax: float
+        :param vmin: minimum value of the contour levels
+        :type vmin: float
+        :param lines: when set to True, overplot the solid contour levels
+        :type lines: bool
+        :param normed: when set to True, the colormap is centered around zero.
+                       Default is True, except for entropy/temperature plots.
+        :type normed: bool
+        """
+
+        rcmb = self.radius_3D[0]
+        rnorm = self.radius_3D/rcmb
+        mask = np.where(abs(rnorm-r) == abs(rnorm-r).min(), 1, 0)
+        irad = np.nonzero(mask)[0][0]
+        if field in ('temperature', 'Temperature', 'temp', 'Temp', 't', 'T'):
+            data = self.temp[:, :, irad]
+        elif field in ('tfluct', 'tempfluct'):
+            data = self.temp-self.temp.mean(axis=0)
+            data = data[:, :, irad]
+        elif field in ('ur', 'Ur', 'Vr', 'vr'):
+            data = self.ur[:, :, irad]
+        elif field in ('ut', 'Ut', 'utheta', 'Utheta', 'vt', 'Vt', 'Vtheta',
+                       'vtheta'):
+            data = self.utheta[:, :, irad]
+        elif field in ('up', 'Up', 'uphi', 'Uphi', 'vp', 'Vp', 'Vphi', 'vphi'):
+            data = self.uphi[:, :, irad]
+
+        if deminc:
+            data = symmetrize(data, ms=self.minc_3D)
+
+        fig = radialContour(data, rad=r, label=label, lon_0=lon_0, vmax=vmax,
+                            vmin=vmin, levels=levels, cm=cm, normed=normed,
+                            cbar=cbar, tit=False, lines=lines)
--- python/pizza/plotlib.py
+++ python/pizza/plotlib.py
@@ -31,6 +31,42 @@ def cut(dat, vmax=None, vmin=None):
     return dat
 
 
+def hammer2cart(ttheta, pphi, colat=False):
+    """
+    This function is used to define the Hammer projection used when
+    plotting surface contours in :py:class:`magic.Surf`
+
+    >>> # Load Graphic file
+    >>> gr = MagicGraph()
+    >>> # Meshgrid
+    >>> pphi, ttheta = mgrid[-np.pi:np.pi:gr.nphi*1j, np.pi/2.:-np.pi/2.:gr.ntheta*1j]
+    >>> x,y = hammer2cart(ttheta, pphi)
+    >>> # Contour plots
+    >>> contourf(x, y, gr.vphi)
+
+    :param ttheta: meshgrid [nphi, ntheta] for the latitudinal direction
+    :type ttheta: numpy.ndarray
+    :param pphi: meshgrid [nphi, ntheta] for the azimuthal direction
+    :param colat: colatitudes (when not specified a regular grid is assumed)
+    :type colat: numpy.ndarray
+    :returns: a tuple that contains two [nphi, ntheta] arrays: the x, y
+              meshgrid used in contour plots
+    :rtype: (numpy.ndarray, numpy.ndarray)
+    """
+
+    if not colat: # for lat and phi \in [-pi, pi]
+        xx = 2.*np.sqrt(2.) * np.cos(ttheta)*np.sin(pphi/2.)\
+             /np.sqrt(1.+np.cos(ttheta)*np.cos(pphi/2.))
+        yy = np.sqrt(2.) * np.sin(ttheta)\
+             /np.sqrt(1.+np.cos(ttheta)*np.cos(pphi/2.))
+    else:  # for colat and phi \in [0, 2pi]
+        xx = -2.*np.sqrt(2.) * np.sin(ttheta)*np.cos(pphi/2.)\
+             / np.sqrt(1.+np.sin(ttheta)*np.sin(pphi/2.))
+        yy = np.sqrt(2.) * np.cos(ttheta) / \
+             np.sqrt(1.+np.sin(ttheta)*np.sin(pphi/2.))
+    return xx, yy
+
+
 def equatContour(data, radius, minc=1, label=None, levels=65,
                  cm='seismic', normed=True, vmax=None, vmin=None, cbar=True,
                  tit=True, normRad=False, deminc=True, stream=False,
@@ -170,6 +206,225 @@ def equatContour(data, radius, minc=1, label=None, levels=65,
                     max(abs(data.max()), abs(data.min())))
 
     return fig, xx, yy
+
+
+def merContour(data, radius, colat=None, label=None, levels=65, cm='seismic',
+               normed=True, vmax=None, vmin=None, cbar=True, tit=True):
+    """
+    Plot a meridional cut of a given field
+
+    :param data: the input data (an array of size (ntheta,nr))
+    :type data: numpy.ndarray
+    :param radius: the input radius
+    :type radius: numpy.ndarray
+    :param colat: the input colatitude
+    :type colat: numpy.ndarray
+    :param label: the name of the input physical quantity you want to
+                  display
+    :type label: str
+    :param levels: the number of levels in the contour
+    :type levels: int
+    :param cm: name of the colormap ('jet', 'seismic', 'RdYlBu_r', etc.)
+    :type cm: str
+    :param tit: display the title of the figure when set to True
+    :type tit: bool
+    :param cbar: display the colorbar when set to True
+    :type cbar: bool
+    :param vmax: maximum value of the contour levels
+    :type vmax: float
+    :param vmin: minimum value of the contour levels
+    :type vmin: float
+    :param normed: when set to True, the colormap is centered around zero.
+                   Default is True, except for entropy/temperature plots.
+    :type normed: bool
+    """
+    ntheta, nr = data.shape
+
+    if colat is not None:
+        th = colat
+    else:
+        th = np.linspace(0, np.pi, ntheta)
+    rr, tth = np.meshgrid(radius, th)
+    xx = rr * np.sin(tth)
+    yy = rr * np.cos(tth)
+
+    if tit and label is not None:
+        if cbar:
+            fig = plt.figure(figsize=(5, 7.5))
+            ax = fig.add_axes([0.01, 0.01, 0.69, 0.91])
+        else:
+            fig = plt.figure(figsize=(3.5, 7.5))
+            ax = fig.add_axes([0.01, 0.01, 0.98, 0.91])
+        ax.set_title(label, fontsize=24)
+    else:
+        if cbar:
+            fig = plt.figure(figsize=(5, 7))
+            ax = fig.add_axes([0.01, 0.01, 0.69, 0.98])
+        else:
+            fig = plt.figure(figsize=(3.5, 7))
+            ax = fig.add_axes([0.01, 0.01, 0.98, 0.98])
+
+    cmap = plt.get_cmap(cm)
+    if vmax is not None and vmin is not None:
+        normed = False
+        cs = np.linspace(vmin, vmax, levels)
+        im = ax.contourf(xx, yy, data, cs, cmap=cmap, extend='both')
+    else:
+        cs = levels
+        im = ax.contourf(xx, yy, data, cs, cmap=cmap)
+
+    for c in im.collections:
+        c.set_edgecolor("face")
+
+    ax.plot((radius[0])*np.sin(th), (radius[0])*np.cos(th), 'k-')
+    ax.plot((radius[-1])*np.sin(th), (radius[-1])*np.cos(th), 'k-')
+    ax.plot([0., 0.], [radius[-1], radius[0]], 'k-')
+    ax.plot([0., 0.], [-radius[-1], -radius[0]], 'k-')
+
+    eps = 1e-4
+    if xx.min() < -eps:
+        ax.set_xlim(1.01*xx.min(), 1.01*xx.max())
+    elif abs(xx.min()) < eps:
+        ax.set_xlim(xx.min()-0.01, 1.01*xx.max())
+    else:
+        ax.set_xlim(0.99*xx.min(), 1.01*xx.max())
+    if yy.min() < -eps:
+        ax.set_ylim(1.01*yy.min(), 1.01*yy.max())
+    elif abs(yy.min()) < eps:
+        ax.set_ylim(yy.min()-0.01, 1.01*yy.max())
+    else:
+        ax.set_ylim(0.99*yy.min(), 1.01*yy.max())
+    ax.axis('off')
+
+    # Add the colorbar at the right place
+    pos = ax.get_position()
+    l, b, w, h = pos.bounds
+    if cbar:
+        if tit and label is not None:
+            cax = fig.add_axes([0.82, 0.46-0.7*h/2., 0.04, 0.7*h])
+        else:
+            cax = fig.add_axes([0.82, 0.5-0.7*h/2., 0.04, 0.7*h])
+        fig.colorbar(im, cax=cax)
+
+    # Normalisation of the contours around zero
+    if normed is True:
+        im.set_clim(-max(abs(data.max()), abs(data.min())),
+                    max(abs(data.max()), abs(data.min())))
+
+    return fig, xx, yy
+
+
+def radialContour(data, rad=0.85, label=None, lon_0=0., vmax=None,
+                  vmin=None, lat_0=30., levels=65, cm='seismic',
+                  normed=True, cbar=True, tit=True, lines=False):
+    """
+    Plot the radial cut of a given field
+
+    :param data: the input data (an array of size (nphi,ntheta))
+    :type data: numpy.ndarray
+    :param rad: the value of the selected radius
+    :type rad: float
+    :param label: the name of the input physical quantity you want to
+                  display
+    :type label: str
+    :param levels: the number of levels in the contour
+    :type levels: int
+    :param cm: name of the colormap ('jet', 'seismic', 'RdYlBu_r', etc.)
+    :type cm: str
+    :param tit: display the title of the figure when set to True
+    :type tit: bool
+    :param cbar: display the colorbar when set to True
+    :type cbar: bool
+    :param lines: when set to True, over-plot solid lines to highlight
+                  the limits between two adjacent contour levels
+    :type lines: bool
+    :param vmax: maximum value of the contour levels
+    :type vmax: float
+    :param vmin: minimum value of the contour levels
+    :type vmin: float
+    :param normed: when set to True, the colormap is centered around zero.
+                   Default is True, except for entropy/temperature plots.
+    :type normed: bool
+    """
+
+    nphi, ntheta = data.shape
+
+    phi = np.linspace(-np.pi, np.pi, nphi)
+    theta = np.linspace(np.pi/2, -np.pi/2, ntheta)
+    pphi, ttheta = np.mgrid[-np.pi:np.pi:nphi*1j, np.pi/2.:-np.pi/2.:ntheta*1j]
+    lon2 = pphi * 180./np.pi
+    lat2 = ttheta * 180./np.pi
+
+    circles = np.r_[-60., -30., 0., 30., 60.]
+    delon = 60.
+    meridians = np.arange(-180+delon, 180, delon)
+
+    if tit and label is not None:
+        if cbar:
+            fig = plt.figure(figsize=(9, 4.5))
+            ax = fig.add_axes([0.01, 0.01, 0.87, 0.87])
+        else:
+            fig = plt.figure(figsize=(8, 4.5))
+            ax = fig.add_axes([0.01, 0.01, 0.98, 0.87])
+        ax.set_title('%s: r/ro = %.3f' % (label, rad),
+                     fontsize=24)
+    else:
+        if cbar:
+            fig = plt.figure(figsize=(9, 4))
+            ax = fig.add_axes([0.01, 0.01, 0.87, 0.98])
+        else:
+            fig = plt.figure(figsize=(8, 4))
+            ax = fig.add_axes([0.01, 0.01, 0.98, 0.98])
+
+    x, y = hammer2cart(ttheta, pphi)
+    for lat0 in circles:
+        x0, y0 = hammer2cart(lat0*np.pi/180., phi)
+        ax.plot(x0, y0, 'k:', linewidth=0.7)
+    for lon0 in meridians:
+        x0, y0 = hammer2cart(theta, lon0*np.pi/180.)
+        ax.plot(x0, y0, 'k:', linewidth=0.7)
+    xxout, yyout = hammer2cart(theta, -np.pi-1e-3)
+    xxin, yyin = hammer2cart(theta, np.pi+1e-3)
+    ax.plot(xxin, yyin, 'k-')
+    ax.plot(xxout, yyout, 'k-')
+    ax.axis('off')
+
+    cmap = plt.get_cmap(cm)
+
+    if vmax is not None or vmin is not None:
+        normed = False
+        cs = np.linspace(vmin, vmax, levels)
+        im = ax.contourf(x, y, data, cs, cmap=cmap, extend='both')
+        if lines:
+            ax.contour(x, y, data, cs, colors=['k'], linewidths=0.5,
+                       extend='both', linestyles=['-'])
+    else:
+        cs = levels
+        im = ax.contourf(x, y, data, cs, cmap=cmap)
+        if lines:
+            ax.contour(x, y, data, cs, colors=['k'], linewidths=0.5,
+                       linestyles=['-'])
+
+    # Add the colorbar at the right place
+    pos = ax.get_position()
+    l, b, w, h = pos.bounds
+    if cbar:
+        if tit and label is not None:
+            cax = fig.add_axes([0.9, 0.46-0.7*h/2., 0.03, 0.7*h])
+        else:
+            cax = fig.add_axes([0.9, 0.51-0.7*h/2., 0.03, 0.7*h])
+        mir = fig.colorbar(im, cax=cax)
+
+    # Normalise around zero
+    if normed:
+        im.set_clim(-max(abs(data.max()), abs(data.min())),
+                    max(abs(data.max()), abs(data.min())))
+
+    # To avoid white lines on pdfs
+    for c in im.collections:
+        c.set_edgecolor("face")
+
+    return fig
 
 
 def spec2D(field_m, n_cheb_max=None):
