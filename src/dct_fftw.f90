e081789452545069f89c5fd56bc17e7df3c289fa
Thomas Gastine <gastine@ipgp.fr>
2024-10-08 14:13:08 +0200

    implement support for DFT loop/many to compute the DCTs

--- src/dct_fftw.f90
+++ src/dct_fftw.f90
@@ -1,3 +1,7 @@
+#define dct_many 0
+#define dft_loop 1
+#define dft_many 2
+#define DCT_VERSION dft_loop
 module dct_fftw
 
    use iso_c_binding
@@ -18,7 +22,13 @@ module dct_fftw
 
    type, public :: costf_t
       real(cp) :: cheb_fac
+#if   (DCT_VERSION==dct_many)
       type(c_ptr) :: plan
+#elif (DCT_VERSION==dft_loop)
+      type(c_ptr) :: plan_fft_back, plan_fft_forw   ! FFTW single plan for FFT
+#elif (DCT_VERSION==dft_many)
+      type(c_ptr) :: plan_fft_forw
+#endif
       type(c_ptr) :: plan_1d
    contains
       procedure :: initialize
@@ -33,22 +43,52 @@ contains
    subroutine initialize(this, nMstart, nMstop, n_r_max, no_work_array)
 
       class(costf_t) :: this
-      
+
       integer, intent(in) :: nMstart
       integer, intent(in) :: nMstop
       integer, intent(in) :: n_r_max
       logical, optional, intent(in) :: no_work_array
 
       !--Local variables
-      integer :: inembed(1), istride, idist, plan_size(1)
-      integer :: onembed(1), ostride, odist
+      integer :: plan_size(1)
       integer(C_INT) :: plan_type(1)
-      real(cp) :: array_in(1:2*(nMstop-nMstart+1), n_r_max)
-      real(cp) :: array_out(1:2*(nMstop-nMstart+1), n_r_max)
-      real(cp) :: array_in_1d(n_r_max)
-      real(cp) :: array_out_1d(n_r_max)
+#if   (DCT_VERSION==dft_loop)
+      complex(cp) :: array_cplx_1d(2*n_r_max-2), array_cplx_out_1d(2*n_r_max-2)
+#elif (DCT_VERSION==dct_many)
+      integer :: inembed(1), istride, idist
+      integer :: onembed(1), ostride, odist
+      real(cp) :: array_in(2*(nMstop-nMstart+1), n_r_max)
+      real(cp) :: array_out(2*(nMstop-nMstart+1), n_r_max)
+#elif (DCT_VERSION==dft_many)
+      integer :: inembed(1), istride, idist
+      integer :: onembed(1), ostride, odist
+      complex(cp) :: array_in(nMstop-nMstart+1, 2*n_r_max-2)
+      complex(cp) :: array_out(nMstop-nMstart+1, 2*n_r_max-2)
+#endif
+      real(cp) :: array_in_1d(n_r_max), array_out_1d(n_r_max)
       logical :: l_work_array
 
+#if   (DCT_VERSION==dft_loop)
+      plan_size(1) = 2*n_r_max-2
+      this%plan_fft_back = fftw_plan_dft(1, plan_size, array_cplx_1d,      &
+                           &             array_cplx_out_1d, FFTW_BACKWARD, &
+                           &             fftw_plan_flag)
+      this%plan_fft_forw = fftw_plan_dft(1, plan_size, array_cplx_1d,      &
+                           &             array_cplx_out_1d, FFTW_FORWARD,  &
+                           &             fftw_plan_flag)
+#elif (DCT_VERSION==dft_many)
+      plan_size(1) = 2*n_r_max-2
+      inembed(1) = 0
+      onembed(1) = 0
+      istride = nMstop-nMstart+1
+      ostride = nMstop-nMstart+1
+      idist   = 1
+      odist   = 1
+      this%plan_fft_forw = fftw_plan_many_dft(1, plan_size, nM_per_rank,          &
+                           &                  array_in, inembed, istride, idist,  &
+                           &                  array_out, onembed, ostride, odist, &
+                           &                  FFTW_FORWARD,fftw_plan_flag)
+#elif (DCT_VERSION==dct_many)
       inembed(1) = 0
       onembed(1) = 0
       plan_size(1) = n_r_max
@@ -62,7 +102,10 @@ contains
                   &                  inembed, istride, idist, array_out,    &
                   &                  onembed, ostride, odist,               &
                   &                  plan_type, fftw_plan_flag)
+#endif
 
+      plan_size(1) = n_r_max
+      plan_type(1) = FFTW_REDFT00
       this%plan_1d = fftw_plan_r2r(1, plan_size, array_in_1d, array_out_1d, &
                      &             plan_type, fftw_plan_flag)
 
@@ -105,7 +148,14 @@ contains
       end if
 
       call fftw_destroy_plan(this%plan_1d)
+#if   (DCT_VERSION==dct_many)
       call fftw_destroy_plan(this%plan)
+#elif (DCT_VERSION==dft_loop)
+      call fftw_destroy_plan(this%plan_fft_back)
+      call fftw_destroy_plan(this%plan_fft_forw)
+#elif (DCT_VERSION==dft_many)
+      call fftw_destroy_plan(this%plan_fft_forw)
+#endif
 
    end subroutine finalize
 !------------------------------------------------------------------------------
@@ -118,21 +168,59 @@ contains
       integer, intent(in) :: nMstop
       integer, intent(in) :: n_r_max
 
+#if (DCT_VERSION==dft_loop)
+      !-- Output variables:
+      complex(cp), intent(inout) :: array_in(nMstart:nMstop,n_r_max)
+
+      !-- Local variables:
+      integer :: n_m
+      complex(cp) :: work_1d(2*n_r_max-2), work_1d_out(2*n_r_max-2)
+
+      do n_m=nMstart,nMstop
+         work_1d(1:n_r_max) = array_in(n_m,1:n_r_max)
+         work_1d(n_r_max+1:)= array_in(n_m,n_r_max-1:2:-1)
+         call fftw_execute_dft(this%plan_fft_forw, work_1d, work_1d_out)
+         array_in(n_m,:)=this%cheb_fac*work_1d_out(1:n_r_max)
+      end do
+
+#elif (DCT_VERSION==dft_many)
+      !-- Output variables:
+      complex(cp), intent(inout) :: array_in(nMstart:nMstop,n_r_max)
+
+      !-- Local variables:
+      integer :: n_r
+      complex(cp) :: tmp_in(nMstart:nMstop,2*n_r_max-2)
+      complex(cp) :: tmp_out(nMstart:nMstop,2*n_r_max-2)
+
+      do n_r=1,n_r_max
+         tmp_in(:,n_r)=array_in(:,n_r)
+      end do
+      do n_r=n_r_max+1,2*n_r_max-2
+         tmp_in(:,n_r)=array_in(:,2*n_r_max-n_r)
+      end do
+
+      call fftw_execute_dft(this%plan_fft_forw, tmp_in, tmp_out)
+
+      !-- Copy output onto a
+      do n_r=1,n_r_max
+         array_in(:,n_r)=this%cheb_fac*tmp_out(:,n_r)
+      end do
+
+#elif (DCT_VERSION==dct_many)
       !-- Output variables:
       complex(cp), target, intent(inout) :: array_in(nMstart:nMstop,n_r_max)
 
       !-- Local variables
+      integer :: n_r
       real(cp), pointer :: r_input(:,:)
-      integer :: n_r, n_m
 
       call c_f_pointer(c_loc(array_in), r_input, [2*nm_per_rank, n_r_max])
       call fftw_execute_r2r(this%plan, r_input, work_r)
 
       do n_r=1,n_r_max
-         do n_m=nMstart,nMstop
-            array_in(n_m,n_r)=this%cheb_fac*work(n_m,n_r)
-         end do
+         array_in(nMstart:nMstop,n_r)=this%cheb_fac*work(nMstart:nMstop,n_r)
       end do
+#endif
 
    end subroutine costf_complex_2d
 !------------------------------------------------------------------------------
@@ -148,13 +236,9 @@ contains
 
       !-- Local variables
       real(cp) :: work_1d(n_r_max)
-      integer :: n_r
 
       call fftw_execute_r2r(this%plan_1d, array_in, work_1d)
-
-      do n_r=1,n_r_max
-         array_in(n_r)=this%cheb_fac*work_1d(n_r)
-      end do
+      array_in(:)=this%cheb_fac*work_1d(:)
 
    end subroutine costf_real_1d
 !------------------------------------------------------------------------------
--- src/radial_derivatives.f90
+++ src/radial_derivatives.f90
@@ -8,6 +8,8 @@ module radial_der
    use precision_mod
    use mem_alloc
    use radial_scheme, only: type_rscheme
+   use chebyshev, only: type_cheb
+   use finite_differences, only: type_fd
    use useful, only: abortRun
 
    implicit none
@@ -79,9 +81,9 @@ contains
 !------------------------------------------------------------------------------
    subroutine get_dcheb_complex_2d(f,df,nMstart,nMstop,n_r_max,n_cheb_max)
       !
-      !  Returns chebychev coeffitiens of first derivative df and second  
-      !  derivative ddf for a function whose cheb-coeff. are given as     
-      !  columns in array f(nMstart:nMstop,n_r_max).                             
+      !  Returns chebychev coeffitiens of first derivative df and second
+      !  derivative ddf for a function whose cheb-coeff. are given as
+      !  columns in array f(nMstart:nMstop,n_r_max).
       !
 
       !-- Input variables:
@@ -183,22 +185,22 @@ contains
 !------------------------------------------------------------------------------
    subroutine get_ddcheb_complex_2d(f,df,ddf,nMstart,nMstop,n_r_max,n_cheb_max)
       !
-      !  Returns chebychev coefficents of first derivative df and second  
-      !  derivative ddf for a function whose cheb-coeff. are given as     
-      !  columns in array f(n_c_tot,n_r_max).                             
+      !  Returns chebychev coefficents of first derivative df and second
+      !  derivative ddf for a function whose cheb-coeff. are given as
+      !  columns in array f(n_c_tot,n_r_max).
       !
-    
+
       !-- Input variables:
       integer,     intent(in) :: nMstart  ! No of column to start with
       integer,     intent(in) :: nMstop   ! No of column to stop with
       integer,     intent(in) :: n_r_max    ! second dimension of f,df,ddf
       integer,     intent(in) :: n_cheb_max ! Number of cheb modes
       complex(cp), intent(in) :: f(nMstart:nMstop,n_r_max)
-    
+
       !-- Output variables:
       complex(cp), intent(out) ::  df(nMstart:nMstop,n_r_max)
       complex(cp), intent(out) ::  ddf(nMstart:nMstop,n_r_max)
-    
+
       !-- local variables:
       integer :: n_m,n_cheb
       real(cp) :: fac_cheb
@@ -229,7 +231,7 @@ contains
          end if
          ddf(n_m,n_cheb)=zero
       end do
-    
+
       !----- recursion
       do n_cheb=n_cheb_max-2,1,-1
          fac_cheb=real(2*n_cheb,kind=cp)
@@ -251,26 +253,26 @@ contains
 !------------------------------------------------------------------------------
    subroutine get_ddcheb_real_1d(f,df,ddf,n_r_max,n_cheb_max)
       !
-      !  Returns chebychev coefficents of first derivative df and second  
-      !  derivative ddf for a function whose cheb-coeff. are given as     
-      !  columns in array f(n_c_tot,n_r_max).                             
+      !  Returns chebychev coefficents of first derivative df and second
+      !  derivative ddf for a function whose cheb-coeff. are given as
+      !  columns in array f(n_c_tot,n_r_max).
       !
-    
+
       !-- Input variables:
       integer,  intent(in) :: n_r_max    ! second dimension of f,df,ddf
       integer,  intent(in) :: n_cheb_max ! Number of cheb modes
       real(cp), intent(in) :: f(n_r_max)
-    
+
       !-- Output variables:
       real(cp), intent(out) ::  df(n_r_max)
       real(cp), intent(out) ::  ddf(n_r_max)
-    
+
       !-- local variables:
       integer :: n_cheb
       real(cp) :: fac_cheb, threshold
 
       threshold = maxval(abs(f))*thr
-    
+
       !----- initialize derivatives:
       do n_cheb=n_cheb_max,n_r_max
          df(n_cheb) =0.0_cp
@@ -288,7 +290,7 @@ contains
          df(n_cheb) =0.0_cp
       end if
       ddf(n_cheb)=0.0_cp
-    
+
       !----- recursion
       do n_cheb=n_cheb_max-2,1,-1
          fac_cheb=real(2*n_cheb,kind=cp)
@@ -310,13 +312,13 @@ contains
    subroutine get_dr_complex_2d(f,df,nMstart,nMstop,n_r_max,r_scheme,nocopy,  &
               &                 l_dct_in,l_dct_out)
       !
-      !  Returns first radial derivative df of the input function f.      
-      !  Array f(nMstart:nMstop,*) may contain several functions numbered by     
-      !  the first index. The subroutine calculates the derivaties of     
-      !  the functions f(nMstart,*) to f(nMstop) by transforming      
-      !  to a Chebychev representation using n_r_max radial grid points . 
+      !  Returns first radial derivative df of the input function f.
+      !  Array f(nMstart:nMstop,*) may contain several functions numbered by
+      !  the first index. The subroutine calculates the derivaties of
+      !  the functions f(nMstart,*) to f(nMstop) by transforming
+      !  to a Chebychev representation using n_r_max radial grid points .
       !
-    
+
       !-- Input variables:
       integer,             intent(in) :: n_r_max  ! number of radial grid points
       integer,             intent(in) :: nMstart  ! first function to be treated
@@ -326,48 +328,48 @@ contains
       logical, optional,   intent(in) :: nocopy
       logical, optional,   intent(in) :: l_dct_in
       logical, optional,   intent(in) :: l_dct_out
-    
+
       !-- Output variables:
       complex(cp), intent(out) :: df(nMstart:nMstop,n_r_max) ! first derivative of f
-    
+
       !-- Local:
-      integer :: n_r,n_f,od
+      integer :: n_r,od
       logical :: copy_array, l_dct_in_loc, l_dct_out_loc
 
-      if ( present(l_dct_in) ) then
-         l_dct_in_loc=l_dct_in
-      else
-         l_dct_in_loc=.true.
-      end if
+      select type ( r_scheme)
 
-      if ( present(l_dct_out) ) then
-         l_dct_out_loc=l_dct_out
-      else
-         l_dct_out_loc=.true.
-      end if
-    
-      if ( r_scheme%version == 'cheb' ) then
+      type is(type_cheb )
+
+         if ( present(l_dct_in) ) then
+            l_dct_in_loc=l_dct_in
+         else
+            l_dct_in_loc=.true.
+         end if
+
+         if ( present(l_dct_out) ) then
+            l_dct_out_loc=l_dct_out
+         else
+            l_dct_out_loc=.true.
+         end if
 
          if ( present(nocopy) ) then
             copy_array=.false.
          else
             copy_array=.true.
          end if
-    
+
          if ( copy_array )  then
             do n_r=1,n_r_max
-               do n_f=nMstart,nMstop
-                  work(n_f,n_r)=f(n_f,n_r)
-               end do
+               work(:,n_r)=f(:,n_r)
             end do
-       
+
             !-- Transform f to cheb space:
             if ( l_dct_in_loc ) call r_scheme%costf1(work,nMstart,nMstop,n_r_max)
-          
+
             !-- Get derivatives:
             !call get_dcheb(work,df,nMstart,nMstop,n_r_max,r_scheme%n_max)
             call get_dcheb(work,df,nMstart,nMstop,n_r_max,n_r_max)
-          
+
             !-- Transform back:
             if ( l_dct_out_loc ) call r_scheme%costf1(df,nMstart,nMstop,n_r_max)
 
@@ -375,109 +377,93 @@ contains
 
             !-- Transform f to cheb space:
             if ( l_dct_in_loc ) call r_scheme%costf1(f,nMstart,nMstop,n_r_max)
-          
+
             !-- Get derivatives:
             !call get_dcheb(f,df,nMstart,nMstop,n_r_max,r_scheme%n_max)
             call get_dcheb(f,df,nMstart,nMstop,n_r_max,n_r_max)
-          
+
             !-- Transform back:
             if ( l_dct_in_loc ) call r_scheme%costf1(f,nMstart,nMstop,n_r_max)
             if ( l_dct_out_loc ) call r_scheme%costf1(df,nMstart,nMstop,n_r_max)
 
          end if
-       
+
          !-- New map:
          do n_r=1,n_r_max
-            do n_f=nMstart,nMstop
-               df(n_f,n_r)=r_scheme%drx(n_r)*df(n_f,n_r)
-            end do
+            df(:,n_r)=r_scheme%drx(n_r)*df(:,n_r)
          end do
 
-      else
+      type is(type_fd)
 
          !-- Initialise to zero:
-         do n_r=1,n_r_max
-            do n_f=nMstart,nMstop
-               df(n_f,n_r) =zero
-            end do
-         end do
+         df(:,:) =zero
 
          !-- Bulk points for 1st derivative
          do od=0,r_scheme%order
             do n_r=1+r_scheme%order/2,n_r_max-r_scheme%order/2
-               do n_f=nMstart,nMstop
-                  df(n_f,n_r)=df(n_f,n_r)+r_scheme%dr(n_r,od)*f(n_f,n_r-r_scheme%order/2+od)
-               end do
+               df(:,n_r)=df(:,n_r)+r_scheme%dr(n_r,od)*f(:,n_r-r_scheme%order/2+od)
             end do
          end do
 
          !-- Boundary points for 1st derivative
          do od=0,r_scheme%order_boundary
             do n_r=1,r_scheme%order/2
-               do n_f=nMstart,nMstop
-                  df(n_f,n_r) = df(n_f,n_r)+r_scheme%dr_top(n_r,od) * f(n_f,od+1)
-               end do
+               df(:,n_r) = df(:,n_r)+r_scheme%dr_top(n_r,od) * f(:,od+1)
             end do
             do n_r=1,r_scheme%order/2
-               do n_f=nMstart,nMstop
-                  df(n_f,n_r_max-n_r+1) = df(n_f,n_r_max-n_r+1)+               &
-                  &                       r_scheme%dr_bot(n_r,od)*f(n_f,n_r_max-od)
-               end do
+               df(:,n_r_max-n_r+1) = df(:,n_r_max-n_r+1)+               &
+               &                       r_scheme%dr_bot(n_r,od)*f(:,n_r_max-od)
             end do
          end do
 
-      end if
+      end select
 
    end subroutine get_dr_complex_2d
 !------------------------------------------------------------------------------
    subroutine get_dr_real_1d(f,df,n_r_max,r_scheme,l_dct)
-    
+
       !-- Input variables:
       integer,             intent(in) :: n_r_max  ! number of radial grid points
       real(cp),            intent(inout) :: f(n_r_max)
       class(type_rscheme), intent(in) :: r_scheme
       logical, optional,   intent(in) :: l_dct
-    
+
       !-- Output variables:
       real(cp), intent(out) :: df(n_r_max) ! first derivative of f
-    
+
       !-- Local:
       integer :: n_r,od
       logical :: l_dct_loc
 
-      if ( present(l_dct) ) then
-         l_dct_loc=l_dct
-      else
-         l_dct_loc=.true.
-      end if
-    
-      if ( r_scheme%version == 'cheb' ) then
+      select type ( r_scheme)
 
-         do n_r=1,n_r_max
-            work_1d_real(n_r)=f(n_r)
-         end do
-       
+      type is(type_cheb )
+
+         if ( present(l_dct) ) then
+            l_dct_loc=l_dct
+         else
+            l_dct_loc=.true.
+         end if
+
+         work_1d_real(:)=f(:)
+
          !-- Transform f to cheb space:
          if ( l_dct_loc ) call r_scheme%costf1(work_1d_real,n_r_max)
-          
+
          !-- Get derivatives:
          !call get_dcheb(work_1d_real,df,n_r_max,r_scheme%n_max)
          call get_dcheb(work_1d_real,df,n_r_max,n_r_max)
-          
+
          !-- Transform back:
          call r_scheme%costf1(df,n_r_max)
 
          !-- New map:
-         do n_r=1,n_r_max
-            df(n_r)=r_scheme%drx(n_r)*df(n_r)
-         end do
+         df(:)=r_scheme%drx(:)*df(:)
 
-      else
+      type is(type_fd)
 
          !-- Initialise to zero:
-         do n_r=1,n_r_max
-            df(n_r)=0.0_cp
-         end do
+         df(:)=0.0_cp
 
          !-- Bulk points for 1st derivative
          do od=0,r_scheme%order
@@ -497,20 +483,20 @@ contains
             end do
          end do
 
-      end if
+      end select
 
    end subroutine get_dr_real_1d
 !------------------------------------------------------------------------------
    subroutine get_ddr_complex_2d(f,df,ddf,nMstart,nMstop,n_r_max,r_scheme,l_dct)
       !
-      !  Returns first radial derivative df and second radial             
-      !  derivative ddf of the input function f.                          
-      !  Array f(nMstart:nMstop,*) may contain several functions numbered by     
-      !  the first index. The subroutine calculates the derivatives of    
-      !  the functions f(nMstart,*) to f(nMstop) by transforming      
-      !  to a Chebychev representation using n_r_max radial grid points.  
+      !  Returns first radial derivative df and second radial
+      !  derivative ddf of the input function f.
+      !  Array f(nMstart:nMstop,*) may contain several functions numbered by
+      !  the first index. The subroutine calculates the derivatives of
+      !  the functions f(nMstart,*) to f(nMstop) by transforming
+      !  to a Chebychev representation using n_r_max radial grid points.
       !
-    
+
       !-- Input variables:
       integer,             intent(in) :: n_r_max  ! number of radial grid points
       integer,             intent(in) :: nMstart  ! first function to be treated
@@ -518,150 +504,131 @@ contains
       complex(cp),         intent(in) :: f(nMstart:nMstop,n_r_max)
       class(type_rscheme), intent(in) :: r_scheme
       logical, optional,   intent(in) :: l_dct
-    
+
       !-- Output variables:
       complex(cp), intent(out) :: df(nMstart:nMstop,n_r_max) ! first derivative of f
       complex(cp), intent(out) :: ddf(nMstart:nMstop,n_r_max)! second derivative of f
-    
+
       !-- Local variables:
       logical :: l_dct_loc
-      integer :: n_r,n_f,od
+      integer :: n_r,od
 
-      if ( present(l_dct) ) then
-         l_dct_loc=l_dct
-      else
-         l_dct_loc=.true.
-      end if
+      select type ( r_scheme)
 
-      if ( r_scheme%version == 'cheb' ) then
-    
+      type is(type_cheb )
+
+         if ( present(l_dct) ) then
+            l_dct_loc=l_dct
+         else
+            l_dct_loc=.true.
+         end if
+
          !-- Copy input functions:
          do n_r=1,n_r_max
-            do n_f=nMstart,nMstop
-               work(n_f,n_r)=f(n_f,n_r)
-            end do
+            work(:,n_r)=f(:,n_r)
          end do
-    
+
          !-- Transform f to cheb space:
          if ( l_dct_loc ) call r_scheme%costf1(work,nMstart,nMstop,n_r_max)
-    
+
          !-- Get derivatives:
          !call get_ddcheb(work,df,ddf,nMstart,nMstop,n_r_max,r_scheme%n_max)
          call get_ddcheb(work,df,ddf,nMstart,nMstop,n_r_max,n_r_max)
-    
+
          !-- Transform back:
          call r_scheme%costf1(df,nMstart,nMstop,n_r_max)
          call r_scheme%costf1(ddf,nMstart,nMstop,n_r_max)
-    
+
          !-- New map:
          do n_r=1,n_r_max
-            do n_f=nMstart,nMstop
-               ddf(n_f,n_r)=r_scheme%ddrx(n_r)*df(n_f,n_r)+&
-               &            r_scheme%drx(n_r)*r_scheme%drx(n_r)*ddf(n_f,n_r)
-               df(n_f,n_r) =r_scheme%drx(n_r)*df(n_f,n_r)
-            end do
+            ddf(:,n_r)=r_scheme%ddrx(n_r)*df(:,n_r)+&
+            &          r_scheme%drx(n_r)*r_scheme%drx(n_r)*ddf(:,n_r)
+            df(:,n_r) =r_scheme%drx(n_r)*df(:,n_r)
          end do
 
-      else
+      type is(type_fd)
 
          !-- Initialise to zero:
          do n_r=1,n_r_max
-            do n_f=nMstart,nMstop
-               df(n_f,n_r) =zero
-               ddf(n_f,n_r)=zero
-            end do
+            df(:,n_r) =zero
+            ddf(:,n_r)=zero
          end do
 
          !-- Bulk points for 1st and 2nd derivatives
          do od=0,r_scheme%order
             do n_r=1+r_scheme%order/2,n_r_max-r_scheme%order/2
-               do n_f=nMstart,nMstop
-                  df(n_f,n_r)  = df(n_f,n_r) + r_scheme%dr(n_r,od) * f(n_f,n_r-r_scheme%order/2+od)
-                  ddf(n_f,n_r) = ddf(n_f,n_r)+r_scheme%ddr(n_r,od) * f(n_f,n_r-r_scheme%order/2+od)
-               end do
+               df(:,n_r)  = df(:,n_r) + r_scheme%dr(n_r,od) * f(:,n_r-r_scheme%order/2+od)
+               ddf(:,n_r) = ddf(:,n_r)+r_scheme%ddr(n_r,od) * f(:,n_r-r_scheme%order/2+od)
             end do
          end do
 
          !-- Boundary points for 1st derivative
          do od=0,r_scheme%order_boundary
             do n_r=1,r_scheme%order/2
-               do n_f=nMstart,nMstop
-                  df(n_f,n_r) = df(n_f,n_r)+r_scheme%dr_top(n_r,od) * f(n_f,od+1)
-               end do
+               df(:,n_r) = df(:,n_r)+r_scheme%dr_top(n_r,od) * f(:,od+1)
             end do
             do n_r=1,r_scheme%order/2
-               do n_f=nMstart,nMstop
-                  df(n_f,n_r_max-n_r+1) = df(n_f,n_r_max-n_r+1)+               &
-                  &                       r_scheme%dr_bot(n_r,od)*f(n_f,n_r_max-od)
-               end do
+               df(:,n_r_max-n_r+1) = df(:,n_r_max-n_r+1)+               &
+               &                     r_scheme%dr_bot(n_r,od)*f(:,n_r_max-od)
             end do
          end do
 
          !-- Boundary points for 2nd derivative
          do od=0,r_scheme%order_boundary+1
             do n_r=1,r_scheme%order/2
-               do n_f=nMstart,nMstop
-                  ddf(n_f,n_r) = ddf(n_f,n_r)+r_scheme%ddr_top(n_r,od) * f(n_f,od+1)
-               end do
+               ddf(:,n_r) = ddf(:,n_r)+r_scheme%ddr_top(n_r,od) * f(:,od+1)
             end do
             do n_r=1,r_scheme%order/2
-               do n_f=nMstart,nMstop
-                  ddf(n_f,n_r_max-n_r+1) = ddf(n_f,n_r_max-n_r+1)+               &
-                  &                       r_scheme%ddr_bot(n_r,od)*f(n_f,n_r_max-od)
-               end do
+               ddf(:,n_r_max-n_r+1) = ddf(:,n_r_max-n_r+1)+               &
+               &                      r_scheme%ddr_bot(n_r,od)*f(:,n_r_max-od)
             end do
          end do
 
-      end if
+      end select
 
    end subroutine get_ddr_complex_2d
 !------------------------------------------------------------------------------
    subroutine get_ddr_real_1d(f,df,ddf,n_r_max,r_scheme)
-    
+
       !-- Input variables:
       integer,             intent(in) :: n_r_max  ! number of radial grid points
       real(cp),            intent(in) :: f(n_r_max)
       class(type_rscheme), intent(in) :: r_scheme
-    
+
       !-- Output variables:
       real(cp), intent(out) :: df(n_r_max) ! first derivative of f
       real(cp), intent(out) :: ddf(n_r_max)! second derivative of f
-    
+
       !-- Local variables:
       integer :: n_r,od
 
-      if ( r_scheme%version == 'cheb' ) then
-    
+      select type ( r_scheme)
+
+      type is(type_cheb )
+
          !-- Copy input functions:
-         do n_r=1,n_r_max
-            work_1d_real(n_r)=f(n_r)
-         end do
-    
+         work_1d_real(:)=f(:)
+
          !-- Transform f to cheb space:
          call r_scheme%costf1(work_1d_real,n_r_max)
-    
+
          !-- Get derivatives:
          !call get_ddcheb(work_1d_real,df,ddf,n_r_max,r_scheme%n_max)
          call get_ddcheb(work_1d_real,df,ddf,n_r_max,n_r_max)
-    
+
          !-- Transform back:
          call r_scheme%costf1(df,n_r_max)
          call r_scheme%costf1(ddf,n_r_max)
-    
-         !-- New map:
-         do n_r=1,n_r_max
-            ddf(n_r)=r_scheme%ddrx(n_r)*df(n_r)+&
-            &         r_scheme%drx(n_r)*r_scheme%drx(n_r)*ddf(n_r)
-            df(n_r) =r_scheme%drx(n_r)*df(n_r)
-         end do
 
-      else
+         !-- New map:
+         ddf(:)=r_scheme%ddrx(:)*df(:)+r_scheme%drx(:)*r_scheme%drx(:)*ddf(:)
+         df(:) =r_scheme%drx(:)*df(:)
+
+      type is(type_fd)
 
          !-- Initialise to zero:
-         do n_r=1,n_r_max
-            df(n_r) =0.0_cp
-            ddf(n_r)=0.0_cp
-         end do
+         df(:) =0.0_cp
+         ddf(:)=0.0_cp
 
          !-- Bulk points for 1st and 2nd derivatives
          do od=0,r_scheme%order
@@ -693,7 +660,7 @@ contains
             end do
          end do
 
-      end if
+      end select
 
    end subroutine get_ddr_real_1d
 !------------------------------------------------------------------------------
